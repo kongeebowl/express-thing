ffeb357431483e8ae1ad0667b4b549fdaeaf7d74
kongeebowl <kadenpang09@gmail.com>
2026-05-10 01:48:49 -0400

    a

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,1 +1,26 @@
 import type { Request, Response } from "express";
+import User from "../models/user.js";
+
+async function index(req: Request, res: Response) {
+  const users = await User.find();
+  res.send(users);
+}
+
+async function show(req: Request, res: Response) {
+  const id = req.params;
+  const user = await User.findById(id);
+  res.status(201).send(user);
+}
+
+async function destroy(req: Request, res: Response) {
+  const id = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).send({ error: "how is this possible bro" });
+  }
+
+  await user.deleteOne();
+  res.sendStatus(204);
+}
+
+module.exports = { index, show, destroy };
